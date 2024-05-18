d2683757dc8d2c18fe59f2032828455a45eb0981
Aryam Gupta <aryamgupta4@gmail.com>
2024-05-18 11:06:21 +0530

    update 18may 2024

--- src/Components/Footer/FooterUpper.js
+++ src/Components/Footer/FooterUpper.js
@@ -1,157 +1,129 @@
 import React from "react";
 
-import downBtn from './Images/downBtn.svg'
+import downBtn from "./Images/downBtn2.svg";
 
 const FooterUpper = () => {
   return (
     <div className="footerUpperMainDiv">
       <h2>Browse Jobs by:</h2>
       <div className="jobSelectors row container-xxl">
-      <div className="oneJobContainer col-xl-3 col-lg-2.8 col-md-6">
-        <button
-              class="footerDropDownButton"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Job Collections <img src={downBtn}/>
-            </button>
-            <div class="dropdown-menu drop-open" aria-labelledby="dropdownMenuButton">
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              
-             
+        <div className="oneJobContainer col-xl-3 col-lg-2.8 col-md-6">
+          <button
+            class="footerDropDownButton"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Job Collections <img src={downBtn} />
+          </button>
+          <div
+            class="dropdown-menu drop-open"
+            aria-labelledby="dropdownMenuButton"
+          >
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
             </div>
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
+            </div>
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
+            </div>
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
+            </div>
+          </div>
         </div>
         <div className="oneJobContainer col-xl-3 col-lg-2.8 col-md-6">
-        <button
-              class="footerDropDownButton"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Job by Location <img src={downBtn}/>
-            </button>
-            <div class="dropdown-menu drop-open" aria-labelledby="dropdownMenuButton">
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              
-             
+          <button
+            class="footerDropDownButton"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Job by Location <img src={downBtn} />
+          </button>
+          <div
+            class="dropdown-menu drop-open"
+            aria-labelledby="dropdownMenuButton"
+          >
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
             </div>
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
+            </div>
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
+            </div>
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
+            </div>
+          </div>
         </div>
         <div className="oneJobContainer col-xl-3 col-lg-2.8 col-md-6">
-        <button
-              class="footerDropDownButton"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Jobs by Role<img src={downBtn}/>
-            </button>
-            <div class="dropdown-menu drop-open" aria-labelledby="dropdownMenuButton">
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              
-             
+          <button
+            class="footerDropDownButton"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Jobs by Role
+            <img src={downBtn} />
+          </button>
+          <div
+            class="dropdown-menu drop-open"
+            aria-labelledby="dropdownMenuButton"
+          >
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
             </div>
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
+            </div>
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
+            </div>
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
+            </div>
+          </div>
         </div>
         <div className="oneJobContainer col-xl-3 col-lg-2.8 col-md-6">
-        <button
-              class="footerDropDownButton"
-              type="button"
-              id="dropdownMenuButton"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Remote Jobs <img src={downBtn}/>
-            </button>
-            <div class="dropdown-menu drop-open" aria-labelledby="dropdownMenuButton">
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                    one
-                </a>
-              </div>
-              
-             
+          <button
+            class="footerDropDownButton"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Remote Jobs <img src={downBtn} />
+          </button>
+          <div
+            class="dropdown-menu drop-open"
+            aria-labelledby="dropdownMenuButton"
+          >
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
             </div>
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
+            </div>
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
+            </div>
+            <div>
+              <a href="#">Jobs for Bootcamp Grads</a>
+            </div>
+          </div>
         </div>
-
       </div>
     </div>
   );
--- src/Components/Pages/JobDesPage/JobDesPage.js
+++ src/Components/Pages/JobDesPage/JobDesPage.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
+import "./jobdespage.css";
+import forArrow from  './Images/forArrow.svg';
 
 export const JobDesPage = () => {
   return (
-    <div>JobDesPage</div>
-  )
-}
+    <>
+      <div className="container-lg jobDesMainDiv">
+        <div className="companyTopInfo">
+          <div>
+            <h2>Motorola Solutions</h2>
+            <h4>Internship Trainee</h4>
+          </div>
+          <img />
+        </div>
+        <div className="companytags">
+          <label className="experience">0 - 1 yr experience</label>
+          <label className="location">Bangalore</label>
+          <label className="contract">Contract</label>
+          <label className="pay">₹10L - ₹15L /yr</label>
+        </div>
+        <div className="companydescription">
+          <div className="companyLink">
+            <label>Company Website: </label>
+            <a href="#">xyz.com</a>
+          </div>
+          <p className="companyDes">
+            Motorola Solutions Hiring Internship Trainee 2024: Candidates with
+            BS/MS in Computer Science (or equivalent degree) are eligible to
+            apply. The detailed eligibility and application process are given
+            below.
+          </p>
+          <button className="jobApplyBtn">
+            Apply Here
+            <img src={forArrow}/>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
